c3f7649637966e20f6736d18aa016861302e0e7d
siksik-token <siksik222401@gmail.com>
2024-11-03 22:04:11 +0900

    [Bronze V] Title: 고양이, Time: 88 ms, Memory: 13948 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args){
+        // 일반적으로 출력문에서 \(역슬래쉬)는출력할 수 없기 때문에 이스케이프 시퀀스를 사용해야 한다.
+        
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
+
