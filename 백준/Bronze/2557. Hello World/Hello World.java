c3a72e9d1f1d616e620865d6a01178fb47f10c04
siksik-token <siksik222401@gmail.com>
2024-11-03 18:17:51 +0900

    Update Hello World.java

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -1,3 +1,5 @@
+// 백준에서는 자바를 Main.java에서 코드를 작성하는 것으로 가정하기 때문에 class명을 Main으로 정의한다.
+// 추가로 class명은 대문자로 시작하는 것이 관례이다.
 public class Main{
     public static void main(String[] args){
         System.out.print("Hello World!");
